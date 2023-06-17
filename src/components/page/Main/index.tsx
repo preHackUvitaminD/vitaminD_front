cd0fc22b0e405552fb942db678f07a0ab5d4a0b8
WMs784 <wms784.app@gmail.com>
2023-06-17 18:29:01 +0900

    display character

--- src/components/page/Main/index.tsx
+++ src/components/page/Main/index.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export const Main: React.FC = () => {
   return (
     <>
-      <div>
-        <Link href="/ranking">Ranking</Link>
+      <div>UserName:WMs784</div>
+      <div className="flex justify-center mt-4">
+        <Image src="/chara_ex.gif" width={240} height={240} alt="Avatar" />
       </div>
     </>
   )
