8c2dd9eeabb09c95965c2a2d919bf661479eb192
WMs784 <wms784.app@gmail.com>
2023-06-17 23:27:23 +0900

    display level

--- src/components/page/Main/index.tsx
+++ src/components/page/Main/index.tsx
@@ -4,10 +4,11 @@ import Image from 'next/image'
 export const Main: React.FC = () => {
   return (
     <>
-      <div>UserName:WMs784</div>
-      <div className="flex justify-center mt-4">
+      <div className="flex justify-center mt-4">UserName:WMs784</div>
+      <div className="flex justify-center items-center mt-5">
         <Image src="/chara_ex.gif" width={240} height={240} alt="Avatar" />
       </div>
+      <div className="flex justify-center mt-5">Lv.1</div>
     </>
   )
 }
