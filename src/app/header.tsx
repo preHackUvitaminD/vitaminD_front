fad7edc13f96e49ebeb794ed25bf6877587954f4
WMs784 <wms784.app@gmail.com>
2023-06-19 00:50:39 +0900

    change header space

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -10,27 +10,29 @@ export const Header: React.FC = () => {
   return (
     <header>
       <nav className="bg-green-800 w-full">
-        <div className="flex items-center pl-8 h-14">
-          <div className="flex space-x-4">
+        <div className="flex justify-between max-w-6xl mx-auto items-center pl-8 h-14">
+          <div className="flex space-x-50">
             <div className="text-green-100 px-24 py-2">でめたら</div>
-            <Link href="/">
-              <div className="text-green-100 hover:bg-green-700 px-3 py-2 rounded">
-                マイページ
-              </div>
-            </Link>
-            <Link href="/ranking">
-              <div className="text-green-100 hover:bg-green-700 px-3 py-2 rounded">
-                ランキング
-              </div>
-            </Link>
-            <div className="text-green-100 hover:bg-green-700 px-3 py-2 rounded">
-              {token ? (
-                <span></span>
-              ) : (
-                <div>
-                  <button onClick={() => login()}>ログイン</button>
+            <div className="flex ml-auto mr-0">
+              <Link href="/">
+                <div className="text-green-100 hover:bg-green-700 px-3 py-2 rounded ml-auto mr-0">
+                  マイページ
                 </div>
-              )}
+              </Link>
+              <Link href="/ranking">
+                <div className="text-green-100 hover:bg-green-700 px-3 py-2 rounded ml-auto mr-0">
+                  ランキング
+                </div>
+              </Link>
+              <div className="text-green-100 hover:bg-green-700 px-3 py-2 rounded ml-auto mr-0">
+                {token ? (
+                  <span></span>
+                ) : (
+                  <div>
+                    <button onClick={() => login()}>ログイン</button>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         </div>
