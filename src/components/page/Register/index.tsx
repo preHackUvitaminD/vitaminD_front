6a3e4a49dd13e3c56cda3337f1a126e1804bb6d9
kume-negitoro <kume.negitoro@gmail.com>
2023-06-21 17:03:30 +0900

    implement register process

--- src/components/page/Register/index.tsx
+++ src/components/page/Register/index.tsx
@@ -2,11 +2,23 @@ import { useRef } from 'react'
 import { useAuthContext } from '@/app/providers/AuthProvider'
 import { useUserDataContext } from '@/app/providers/UserDataProvider'
 import { redirect } from 'next/navigation'
+import { useMutation } from 'react-query'
+import { register, RegisterProps } from '@/api/backend/register'
 
 export const Register: React.FC = () => {
   const inputRef = useRef<HTMLInputElement>(null)
   const { authData } = useAuthContext()
   const { setUserData, userData } = useUserDataContext()
+
+  const { mutate, isLoading, isError } = useMutation(
+    (prop: RegisterProps) => register(prop),
+    {
+      onSuccess: (data, userData) => {
+        console.log(data)
+        setUserData?.(userData)
+      },
+    }
+  )
 
   const isLogedIn = Boolean(authData?.idToken)
   const isRegistered = Boolean(userData?.groupName)
@@ -17,27 +29,46 @@ export const Register: React.FC = () => {
   if (!isLogedIn) redirect('/signin')
   if (isRegistered) redirect('/')
 
+  if (isLoading) {
+    return <div>Now Loading...</div>
+  }
+
   return (
     <>
-      <form
-        onSubmit={(e) => {
-          // 応急処置
-          e.preventDefault()
+      <div className="flex mt-10">
+        <div className="flex-1"></div>
+        <div className="w-2/3 bg-gray-200/30 backdrop-blur-lg rounded-md border border-gray-200/30 shadow-lg px-32 py-16 text-center">
+          <div className="text-3xl py-5">登録・ログイン</div>
+          {isError && <div>An error occured. Please retry.</div>}
+          <form
+            onSubmit={(e) => {
+              // 応急処置
+              e.preventDefault()
 
-          if (!inputRef.current?.value) return
+              if (!inputRef.current?.value) return
 
-          if (!userData) throw 0
-
-          setUserData?.({
-            userName: userData?.userName!,
-            groupName: inputRef.current!.value,
-          })
-        }}
-      >
-        <div>加入するグループ名を入力してください</div>
-        <input ref={inputRef} type="text" />
-        <input type="submit" value="Register" />
-      </form>
+              mutate({
+                userName: userData?.userName!,
+                groupName: inputRef.current!.value,
+              })
+            }}
+          >
+            <div>加入するグループ名を入力してください</div>
+            <div className="py-4">
+              <input ref={inputRef} type="text" />
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="px-6 py-2 border bg-black text-white rounded-md"
+              >
+                登録
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="flex-1"></div>
+      </div>
     </>
   )
 }
