5559f1a6cfc5f55093f7d6db6d610a470318e49b
Kosei <64195816+WMs784@users.noreply.github.com>
2023-06-22 18:31:17 +0900

    Update src/components/page/Main/index.tsx
    Co-authored-by: negitoro <kume.negitoro@gmail.com>

--- src/components/page/Main/index.tsx
+++ src/components/page/Main/index.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 import Image from 'next/image'
 import { useAuthContext } from '@/providers/AuthProvider'
 import { useUserDataContext } from '@/providers/UserDataProvider'
-import { Character } from '@/components/model/character/Character'
+import { Character } from '@/components/model/character/character'
 
 const OWNER = '<OWNER>'
 const REPO = '<REPO>'
