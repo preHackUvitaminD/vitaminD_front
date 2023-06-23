7933ec8110acb53b424edd7a90975ce7dcd6027b
WMs784 <wms784.app@gmail.com>
2023-06-23 14:22:17 +0900

    implement 5step bar and add message

--- src/components/model/character/Character/Character.tsx
+++ src/components/model/character/Character/Character.tsx
@@ -14,7 +14,8 @@ export const Character: React.FC<CharacterProps> = ({
   const { data } = useFetchRanking({ userName })
   const lv = data?.ranking?.find((user) => user.userName === userName)?.lv
   const exp = data?.ranking?.find((user) => user.userName === userName)?.exp
-  const percentage = exp! / (lv! * 10)
+  const percentage = exp! * 10 - lv! * 100
+  // const percentage = 60
   const lv_m = lv! > 10 ? 10 : lv
 
   return (
@@ -22,9 +23,15 @@ export const Character: React.FC<CharacterProps> = ({
       <div className="flex justify-center items-center mt-32">
         <Image src={`/Lv${lv_m}.gif`} width={280} height={280} alt={''} />
       </div>
-      <div className="text-3xl flex justify-center mt-5">Lv.{lv}</div>
-      <div className="h-10 w-25 justify-center mt-5">
-        <ProgressBar percentWidth={10} percent={percentage!} />
+      <div className="text-3xl flex justify-center mt-10">Lv.{lv}</div>
+      <div className="flex justify-center mt-6">
+        <ProgressBar
+          percentWidth={Math.floor(percentage / 20)}
+          percent={`${percentage / 10}/10`}
+        />
+      </div>
+      <div className="text-xl flex justify-center mt-6">
+        LvUPまであと{(100 - percentage) / 10}
       </div>
     </>
   )
--- src/components/model/character/ExpProgress/ProgressBar.tsx
+++ src/components/model/character/ExpProgress/ProgressBar.tsx
@@ -1,22 +1,18 @@
 export interface Props {
   percentWidth: number
-  percent: number
+  percent: string
 }
 export const ProgressBar = ({ percentWidth, percent }: Props): JSX.Element => {
   return (
     <div className="relative">
       <div className="grid grid-cols-10 text-left">
-        <div className="flex overflow-hidden col-span-8 mx-10 my-2 h-3 text-xs bg-black rounded">
+        <div className="flex overflow-hidden col-span-8 mx-5 my-2 h-3 text-xs bg-black rounded">
           <div
-            className={`w-${String(
-              percentWidth
-            )} flex flex-col justify-center text-center text-white whitespace-nowrap bg-white shadow-none`}
+            className={`w-${percentWidth}/5 flex flex-col justify-center text-center text-white whitespace-nowrap bg-white shadow-none`}
           ></div>
         </div>
         <div className="col-span-2 ml-3">
-          <span className="text-sm font-semibold text-text-red lg:text-base">
-            {percent}%
-          </span>
+          <span className="text-sm font-semibold lg:text-base">{percent}</span>
         </div>
       </div>
     </div>
