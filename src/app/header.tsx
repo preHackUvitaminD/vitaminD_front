93263f88b5cbd075f514bac5f6905ad59ea6f2c0
WMs784 <wms784.app@gmail.com>
2023-06-23 19:50:09 +0900

    modify layout of header

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -10,8 +10,80 @@ export const Header: React.FC = () => {
 
   return (
     <header>
-      <nav className="bg-green-800 w-full">
-        <div className="flex justify-between max-w-6xl mx-auto items-center pl-8 h-14">
+      <nav className="flex items-center justify-between flex-wrap p-3 bg-green-800">
+        <div className="flex items-center flex-shrink-0 text-white mx-6">
+          <span className="font-semibold text-xl tracking-tight">
+            <Link href="/">でめたら</Link>
+          </span>
+        </div>
+        <div className="block lg:hidden">
+          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+            <svg
+              className="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div className="text-sm lg:flex-grow">
+            <a
+              // href="#responsive-header"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              {isLoggedIn && (
+                <Link href="/mypage">
+                  <div className="text-green-100 hover:bg-green-700 px-3 py-2 rounded ml-auto mr-0">
+                    マイページ
+                  </div>
+                </Link>
+              )}
+            </a>
+            <a
+              // href="#responsive-header"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              {isLoggedIn && (
+                <Link href="/ranking">
+                  <div className="text-green-100 hover:bg-green-700 px-3 py-2 rounded ml-auto mr-0">
+                    ランキング
+                  </div>
+                </Link>
+              )}
+            </a>
+            {/* <a
+              href="#responsive-header"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+            >
+              Blog
+            </a> */}
+          </div>
+          <div>
+            <a
+              // href="#"
+              className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:text-teal-500  mt-4 lg:mt-0"
+            >
+              {isLoggedIn ? (
+                <button
+                  onClick={() => logout()}
+                  className="text-green-100 hover:bg-green-700 px-3 py-2 rounded ml-auto mr-0"
+                >
+                  ログアウト
+                </button>
+              ) : (
+                <Link href="/signin">
+                  <div className="text-green-100 hover:bg-green-700 px-3 py-2 rounded ml-auto mr-0">
+                    ログイン
+                  </div>
+                </Link>
+              )}
+            </a>
+          </div>
+        </div>
+        {/* <div className="flex justify-between max-w-6xl mx-auto items-center pl-8 h-14">
           <div className="flex space-x-50">
             <div className="text-green-100 px-24 py-2">
               <Link href="/">でめたら</Link>
@@ -47,7 +119,7 @@ export const Header: React.FC = () => {
               )}
             </div>
           </div>
-        </div>
+        </div> */}
       </nav>
     </header>
   )
